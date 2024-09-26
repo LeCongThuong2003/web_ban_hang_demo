884fd4f7908807500f2a9ccd474a29900b65a46f
LeCongThuong2003 <thuongah01110@gmail.com>
2024-09-26 23:33:04 +0700

    includes

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+
+	<div  id="footerSection">
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				<h5>ACCOUNT</h5>
+				<a href="./profile.php">YOUR ACCOUNT</a>
+				<a href="./profile.php">PERSONAL INFORMATION</a> 
+				<a href="./profile.php">ADDRESSES</a> 
+				<a href="./profile.php">ORDER HISTORY</a>
+			 </div>
+			<div class="span3">
+				<h5>INFORMATION</h5>
+				<a href="./contact.php">CONTACT</a>  
+				<a href="./signup.php">REGISTRATION</a>  
+				<a href="#">LEGAL NOTICE</a>  
+				<a hr#">TERMS AND CONDITIONS</a> 
+				<a hr#">FAQ</a>
+			 </div>
+			<div class="span3">
+				<h5>OUR OFFERS</h5>
+				<a href="./index.php">NEW PRODUCTS</a> 
+				<a href="#">TOP SELLERS</a>  
+				<a href="#">SPECIAL OFFERS</a>  
+				<a href="#">MANUFACTURERS</a> 
+				<a href="#">SUPPLIERS</a> 
+			 </div>
+			<div id="socialMedia" class="span3 pull-right">
+				<h5>SOCIAL MEDIA </h5>
+				<a href="https://www.facebook.com/thuongoh.03/"><img width="60"  height="60" src="images/facebook.png" title="facebook" alt="facebook"/></a>
+				<a href="https://www.instagram.com/thuongdz.03?igsh=MWVqaDRna2c3cTh3ZA=="><img width="60" height="60" src="images/ig.jpg" title="ig" alt="ig"/></a>
+				<a href="https://www.youtube.com/channel/UC5UfFXAMlFs6pqFMCqgmLRQ"><img width="60" height="60" src="images/youtube.png" title="youtube" alt="youtube"/></a>
+			 </div> 
+		 </div>
+		<p class="pull-right"><?php echo $_SESSION['web-footer'];?></p>
+	</div><!-- Container End -->
+	</div>
